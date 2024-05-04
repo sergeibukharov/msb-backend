00358976af5a8ad2f5e04e54b5584cda5efd78d7
Sergey Bukharov <bukharovSI@gmail.com>
2024-05-04 12:05:40 +0800

    Add support of testContainers to SpringBootTest

--- src/test/java/com/thoughtworks/msb/MsbApplicationTests.java
+++ src/test/java/com/thoughtworks/msb/MsbApplicationTests.java
@@ -2,12 +2,22 @@ package com.thoughtworks.msb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-//@SpringBootTest
-//class MsbApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+@Testcontainers
+class MsbApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgresContainer = new  PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+
+	@Test
+	void contextLoads() {
+	}
+
+}
